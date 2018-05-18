adabfb3328cfec41b7ea55679267e7ec5cf6c71c
Yang Fan <yang.fan@igt.com>
2018-05-17 19:33:38 -0500

    Remove redundant folder.

--- src/walmart/chatbot/utilities/InputFormatter.java
+++
@@ -1,8 +0,0 @@
-package walmart.chatbot.utilities;
-
-
-public class InputFormatter {
-    public String format(String input) {
-        return input.isEmpty()|| input.trim().length() == 0 ? "h": input.trim().toLowerCase().replaceAll("\\s+", " ");
-    }
-}
